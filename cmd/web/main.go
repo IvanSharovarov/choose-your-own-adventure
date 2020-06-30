478510a658aabbebdd53b6e0a400323a54e50bb1
Ivan Sharovarov <sharovarov31@gmail.com>
2020-06-30 22:39:56 +0600

    custom paths

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	choose_your_own_adventure "github.com/IvanSharovarov/choose-your-own-adventure"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -25,7 +27,42 @@ func main() {
 		panic(err)
 	}
 
-	h := choose_your_own_adventure.NewHandler(story)
+	tpl := template.Must(template.New("").Parse(storyTmpl))
+
+	h := choose_your_own_adventure.NewHandler(story,
+		choose_your_own_adventure.WithTemplate(tpl),
+		choose_your_own_adventure.WithPathFunc(pathFn),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
 	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h), mux)
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose your own adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+        <p>{{.}}</p>
+    {{end}}
+    {{range .Options}}
+        <li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+</body>
+</html>`
